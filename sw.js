0a9441692063a90343b0cbe7f3c57161c7800494
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2026-07-01 21:24:59 +0700

    feat: add basic PWA support

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'exercises-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
